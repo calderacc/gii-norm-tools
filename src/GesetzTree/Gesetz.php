1fca07ab6391c6671dfe2663bcbc64498449bafb
Malte Hübner <maltehuebner@gmx.org>
2016-12-15 00:58:14 +0100

    Store Paragraphs in Gesetz.

--- src/GesetzTree/Gesetz.php
+++ src/GesetzTree/Gesetz.php
@@ -4,5 +4,17 @@ namespace Caldera\GiiNormTools\GesetzTree;
 
 class Gesetz
 {
+    protected $paragraphList = [];
 
+    public function __construct()
+    {
+
+    }
+
+    public function addParagraph(Paragraph $paragraph): Gesetz
+    {
+        $this->paragraphList[$paragraph->getNummer()] = $paragraph;
+
+        return $this;
+    }
 }
