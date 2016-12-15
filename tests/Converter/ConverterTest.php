89ea05175af0b09d0211127db9fd10e94ce10d62
Malte Hübner <maltehuebner@gmx.org>
2016-12-15 23:07:33 +0100

    Test Absatz 2.

--- tests/Converter/ConverterTest.php
+++ tests/Converter/ConverterTest.php
@@ -32,4 +32,26 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expectedParagraph, $actualParagraph);
     }
+
+    public function testConverter2()
+    {
+        $filename =  __DIR__ . '/../files/stvo.xml';
+
+        $converter = new Converter();
+
+        $actualAbsatz = $converter
+            ->loadXmlFile($filename)
+            ->convert()
+            ->getGesetz()
+            ->getParagraph('2')
+            ->getAbsatz('4')
+        ;
+
+        $expectedAbsatz = new Absatz(
+            '4',
+            'Mit Fahrrädern muss einzeln hintereinander gefahren werden; nebeneinander darf nur gefahren werden, wenn dadurch der Verkehr nicht behindert wird. Eine Pflicht, Radwege in der jeweiligen Fahrtrichtung zu benutzen, besteht nur, wenn dies durch Zeichen 237, 240 oder 241 angeordnet ist. Rechte Radwege ohne die Zeichen 237, 240 oder 241 dürfen benutzt werden. Linke Radwege ohne die Zeichen 237, 240 oder 241 dürfen nur benutzt werden, wenn dies durch das allein stehende Zusatzzeichen „Radverkehr frei“ angezeigt ist. Wer mit dem Rad fährt, darf ferner rechte Seitenstreifen benutzen, wenn keine Radwege vorhanden sind und zu Fuß Gehende nicht behindert werden. Außerhalb geschlossener Ortschaften darf man mit Mofas Radwege benutzen.'
+        );
+
+        $this->assertEquals($expectedAbsatz, $actualAbsatz);
+    }
 }
