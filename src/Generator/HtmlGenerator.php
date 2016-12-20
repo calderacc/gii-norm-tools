b511c55d7b03ba9f70ebc38558dfaeb13537815d
Malte Hübner <maltehuebner@gmx.org>
2016-12-20 02:18:34 +0100

    WIP at generator.

--- src/Generator/HtmlGenerator.php
+++ src/Generator/HtmlGenerator.php
@@ -3,13 +3,38 @@
 namespace Caldera\GiiNormTools\Generator;
 
 use Caldera\GiiNormTools\GesetzTree\Gesetz;
+use Caldera\GiiNormTools\GesetzTree\Paragraph;
 
 class HtmlGenerator
 {
+    /** @var Gesetz $gesetz */
     protected $gesetz;
+
+    protected $html = '';
 
     public function __construct(Gesetz $gesetz)
     {
         $this->gesetz = $gesetz;
+    }
+
+    public function generate()
+    {
+        $this->html .= '<ul>';
+
+        foreach ($this->gesetz->getParagraphList() as $paragraph) {
+            $this->html .= $this->generateParagraph($paragraph);
+        }
+
+        $this->html .= '</ul>';
+    }
+
+    public function getContent(): string
+    {
+        return $this->html;
+    }
+
+    protected function generateParagraph(Paragraph $paragraph): string
+    {
+        return '<li>§ '.$paragraph->getNummer().'</li>';
     }
 }
